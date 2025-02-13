5f3aee3c7e8d738a4f49cc30ec27f24f5d8f714a
RelAthalla <farrelathalla0910@gmail.com>
2025-02-13 12:21:35 +0700

    Feat: Create ProductService.java and ProductServiceImpl.java in service package

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/sevice/ProductService.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.eshop.sevice;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import java.util.List;
+
+public interface ProductService {
+    public Product create(Product product);
+    public List<Product> findAll();
+}
